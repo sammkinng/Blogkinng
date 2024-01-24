6754930621928fd862a6d253b9a3beb3824aab45
sammkinng <fm568pk@gmail.com>
2024-01-24 11:53:59 +0530

    before prisma

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: 'Blogkinng',
+  author: 'Prince Kumar',
+  headerTitle: 'Blogkinng',
+  description: 'Prince Yadav Blog Site',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://blog.sammkinng.in', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'fm568pk@gmail.com',
+  github: 'https://github.com/sammkinng',
+  twitter: 'https://twitter.com/Kinngsamm',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/sammkinng/',
+  Instagram: 'https://www.Instagram.com/sammkinng/',
+  locale: 'en-US',
+}
+
+module.exports = siteMetadata
